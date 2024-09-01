d62fda0a9e0e466bbe72e1c8ea634d3036693c80
Livio <yihan-li21@mails.tsinghua.edu.cn>
2024-09-01 16:52:45 +0800

    实现search函数

--- search.py
+++ search.py
@@ -3,10 +3,18 @@ from bs4 import BeautifulSoup
 from serpapi import GoogleSearch
 
 def search(content: str):
-    """
-    TODO
-    """
-    pass
+    search = GoogleSearch({
+        "q": content,
+        "engine": "bing",
+        "api_key": "05c25def8c46399d93f8b860fc8628d16d5279160256e33f3eabca9f5b3f87b3"
+    })
+    results = search.get_dict()
+    for result in results["organic_results"]:
+        if "snippet" in result:
+            snippet = result["snippet"]
+            return f"Please answer {content} based on the search result: {snippet}"
+        else:
+            return ""
 
 if __name__ == "__main__":
     search("Sun Wukong")
